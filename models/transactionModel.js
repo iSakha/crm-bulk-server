1f323522ee6f55be278b74a550b057d69ff3a39e
iSakha <sakha60@gmail.com>
2022-10-05 21:26:41 +0300

    fixing errors

--- models/transactionModel.js
+++ models/transactionModel.js
@@ -706,7 +706,7 @@ const updateEventShort = (idEvent, eventRow) => {
                                 });
                             };
 
-                            return connection.execute('INSERT INTO `t_events` (idEvent, idWarehouse, title, start, end, idManager_1, idLocation, idClient, idCreatedBy, notes, idStatus, idUpdatedBy, unixTime) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?):', eventRow, err => {
+                            return connection.execute('INSERT INTO `t_events` (idEvent, idWarehouse, title, start, end, idManager_1, idLocation, idClient, idCreatedBy, notes, idStatus, idUpdatedBy, unixTime) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);', eventRow, err => {
                                 if (err) {
                                     return connection.rollback(() => {
                                         connection.release();
@@ -764,7 +764,7 @@ const deleteEvent = (idEvent, eventRow) => {
 
 
 
-                    return connection.execute('INSERT INTO `t_events`(idEvent, idWarehouse, title, start, end, idManager_1, idLocation, idClient, idCreatedBy, createdAt, notes, idStatus,  idUpdatedBy, updatedAt, filledUp, is_deleted, unixTime) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', eventRow, err => {
+                    return connection.execute('INSERT INTO `t_events`(idEvent, idWarehouse, title, start, end, idManager_1, idLocation, idClient, idCreatedBy, createdAt, notes, idStatus,  idUpdatedBy, updatedAt, filledUp, is_deleted, unixTime) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);', eventRow, err => {
                         if (err) {
                             return connection.rollback(() => {
                                 connection.release();
