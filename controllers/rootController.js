0ca1d9eab59ce3dd725db2794bb20cad736ac6f4
iSakha <sakha60@gmail.com>
2022-10-06 20:56:37 +0300

    add requests phase and status

--- controllers/rootController.js
+++ controllers/rootController.js
@@ -45,8 +45,51 @@ exports.getWarehouses = async (req, res) => {
     } else {
         return res.status(status.status).json({ msg: "We have problems with JWT authentication" });
     }
+}
 
 
+exports.getPhases = async (req, res) => {
+
+    try {
+        console.log("getPhases");
+        let status = await auth.authenticateJWT(req, res);
+        console.log("statusCode:", status.status);
+        if (status.status === 200) {
+            const [phase] = await Root.getPhases();
+            console.log(status);
+            phase.shift();
+            res.status(200).json(phase);
+        } else {
+            res.sendStatus(status.status);
+        }
+
+    } catch (error) {
+        if (!error.statusCode) {
+            error.statusCode = 500;
+        }
+    }
+}
+
+exports.getStatus = async (req, res) => {
+
+    try {
+        console.log("getStatus");
+        let status = await auth.authenticateJWT(req, res);
+        console.log("statusCode:", status.status);
+        if (status.status === 200) {
+            const [stats] = await Root.getStatus();
+            console.log(stats);
+            stats.shift();
+            res.status(200).json(stats);
+        } else {
+            res.sendStatus(status.status);
+        }
+
+    } catch (error) {
+        if (!error.statusCode) {
+            error.statusCode = 500;
+        }
+    }
 
 }
 
--- routes/rootRouter.js
+++ routes/rootRouter.js
@@ -6,7 +6,8 @@ const router = express.Router();
 router.get('/', rootController.greetMessage);
 router.get('/checkdb', rootController.checkDbConnection);
 router.get('/warehouses', rootController.getWarehouses);
-
+router.get('/status', rootController.getStatus);
+router.get('/phase', rootController.getPhases);
 
 
 
