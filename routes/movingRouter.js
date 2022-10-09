8b15abe690c7c314bfc7cd4ff55ae8a6e3eace5c
iSakha <sakha60@gmail.com>
2022-10-09 10:08:41 +0300

    get moming status

--- routes/movingRouter.js
+++ routes/movingRouter.js
@@ -7,6 +7,6 @@ router.post('/', movingController.createNew);
 router.get('/:id', movingController.getOne);
 router.put('/:id', movingController.update);
 // router.delete('/:id', movingController.delete);
-// router.get('/status/all', movingController.getStatus);
+router.get('/status/all', movingController.getStatus);
 
 module.exports = router;
