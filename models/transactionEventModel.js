a714fee80bda068ae59a81696d2b663176733b31
iSakha <sakha60@gmail.com>
2022-11-04 22:43:09 +0300

    transaction config

--- models/transactionEventModel.js
+++ models/transactionEventModel.js
@@ -1,13 +1,13 @@
-const db = require('../config/database');
+const config = require('../config/config.json');
 
 const mysql = require('mysql2');
 
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: "eu-cdbr-west-03.cleardb.net",
-    user: "bb962375bd6b35",
-    password: "2d88fb96",
-    database: "heroku_14052c548fadd47"
+    host: config.host,
+    user: config.user,
+    database: config.database,
+    password: config.password
 });
 
 // const pool = mysql.createPool({
